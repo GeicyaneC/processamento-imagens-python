1b47dc43522ff37319ab5cb5af12cbd1c58e7493
Geicyane Clemente <geicyaneclemente@live.com>
2024-10-11 09:04:23 -0300

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -7,14 +7,17 @@ class image():
   def __init__(self,path):
     self.path = np.array(Image.open(path))
 
-  def show_image(self):
-    plt.imshow(self.path)
+
+  def show_image_pillow(self):
+    img = Image.fromarray(self.path)
+    img.show()
 
   def show_array(self):
     print(self.path)
 
   def invert(self):
-    plt.imshow(np.flip(self.path))
+    self.path = np.flip(self.path, axis=1)  # Inverte a imagem horizontalmente
+    self.show_image_pillow()  # Exibe a imagem
 
   def filter(self, color, filter):
         colors = {'r': 0, 'g': 1, 'b': 2}
@@ -36,7 +39,7 @@ class image():
             index = colors[c]
             self.path[:, :, index] = np.clip(self.path[:, :, index] + f, 0, 255)
 
-        self.show_image()
+        self.show_image_pillow()
 
   def weak(self):
     plt.imshow(np.where(self.path == 255, 50, self.path - 10))
